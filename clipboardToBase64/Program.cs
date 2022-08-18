657d6dfd6ccb22f279e8d6d96325cc839ad006a1
Klaus Striessnig <k.striessnig@gmail.com>
2022-08-18 13:47:42 +0200

    Projektdateien hinzufügen.

---
+++ clipboardToBase64/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Thread thread = new(() =>
+{
+    string? file = Main();
+    Thread.Sleep(1000);
+    if (file != null)
+    {
+        File.Delete(file);
+    }
+});
+thread.SetApartmentState(ApartmentState.STA);
+thread.Start();
+
+string? Main()
+{
+    Image? image = Clipboard.GetImage();
+    if (image == null)
+    {
+        return null;
+    }
+    string filePath = SaveImage(image);
+    Process.Start(new ProcessStartInfo()
+    {
+        UseShellExecute = true,
+        CreateNoWindow = true,
+        FileName = "chrome.exe",
+        Arguments = filePath, // or https://lens.google.com/search?p=... I have to check how this works.
+        WindowStyle = ProcessWindowStyle.Hidden,
+    });
+
+    return filePath;
+}
+
+string SaveImage(Image image)
+{
+    string location = Path.GetTempPath();
+    string filePath = Path.Combine(location, Guid.NewGuid().ToString() + ".png");
+    image.Save(filePath);
+    return filePath;
+}
